d8207f113e793a8137edf1e97b6b898adca554d4
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-24 11:44:45 -0400

    feat(router): define root and project detail routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,15 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    component: () => import('@/views/HomePage.vue')
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
+    path: '/proyecto/:id',
+    component: () => import('@/views/ProyectoDetalle.vue'),
+    props: true
   }
 ]
 
